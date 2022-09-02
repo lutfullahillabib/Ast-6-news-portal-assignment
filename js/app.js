9b1eaf170a7ce5a739829e43d593281a51695656
lutfullahillabib <lutfullahillabib@gmail.com>
2022-09-02 21:58:13 +0600

    1st fetch

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const loadNews = async () => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        displayNews(data.data.news_category);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+const displayNews = news => {
+    console.log(news);
+}
+
+loadNews();
